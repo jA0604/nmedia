b283300afcb3f4f0c2bb621ddb001853118fa900
jA0604 <j1107@bk.ru>
2021-05-10 13:49:54 +0300

    dz_2_4 Task Rounded

--- app/src/main/java/ru/netology/nmedia/ui/PostListAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/ui/PostListAdapter.kt
@@ -75,6 +75,7 @@ class PostViewHolder(
                 .placeholder(R.drawable.ic_loading_100dp)
                 .error(R.drawable.ic_error_100dp)
                 .timeout(10_000)
+                .circleCrop()
                 .into(ivAvatar)
 
             ivMore.setOnClickListener {
